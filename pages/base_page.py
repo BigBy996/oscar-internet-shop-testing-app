70f846b8521afbcc3152356b402676f7b5cdf0cd
Aleksandr Khorev <aleksandr.khorev@valantmed.com>
2019-06-25 15:22:12 +0300

    move common methods to base page, refactor for go_to_login_page, fix dependency issue with LoginPage

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,14 +2,26 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
+from pages.locators import BasePageLocators
+
+
 class BasePage(object):
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
 
-    def open(self):
-        self.browser.get(self.url)
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(expected_conditions.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
 
     def is_element_present(self, how, what):
         try:
@@ -25,10 +37,8 @@ class BasePage(object):
             return True
         return False
 
-    def is_disappeared(self, how, what, timeout=4):
-        try:
-            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
-                until_not(expected_conditions.presence_of_element_located((how, what)))
-        except TimeoutException:
-            return False
-        return True
+    def open(self):
+        self.browser.get(self.url)
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
